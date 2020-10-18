8e2bc08bb2721928db69632a13690dc157c05e85
Gkuzin13 <yo.is.josh@gmail.com>
2020-10-18 16:39:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const squareContainer = document.getElementById('squares-ctn');
+
+
+for(i = 1; i <= 256; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square-block');
+    squareContainer.appendChild(square);
+
+    }
+
+
+
+
+
+   
+
+
+

d4dc7b85b91a95ac94d23753b3a7201fe4097a35
Gkuzin13 <yo.is.josh@gmail.com>
2020-10-24 19:10:24 +0300

    all

--- main.js
+++ main.js
@@ -2,33 +2,42 @@
 // Grabs the squares container
 const squareContainer = document.getElementById('squares-ctn');
 
-// Asks the user for a number
-let userInputNumber = prompt('How many squares would you like?');
-// Calls createSquares function
-userInputNumber = createSquares();
+const gridSizeBtn = document.getElementById('grid-size-btn');
+
+let squareNumber = 16;
+
+squareNumber = createSquares();
 
 // Multiplies the user input value by itself 
 function numberMultiply() {
-    return userInputNumber * userInputNumber;
+    return squareNumber * squareNumber;
 };
 
 // Creates a number of squares calculated from users input
 function createSquares() {
-    //
     for(i = 1; i <= numberMultiply(); i++) {
+
         // Creates div 
         let square = document.createElement('div');
+
         // Adds class to the div
         square.classList.add('square-block');
 
         // Changes the amount of Rows/Columns from users input
-        squareContainer.style.gridTemplateRows = `repeat(${userInputNumber}, 1fr)`;
-        squareContainer.style.gridTemplateColumns = `repeat(${userInputNumber}, 1fr)`;
+        squareContainer.style.gridTemplateRows = `repeat(${squareNumber}, 1fr)`;
+        squareContainer.style.gridTemplateColumns = `repeat(${squareNumber}, 1fr)`; 
 
         // Appends created div squares
-        squareContainer.appendChild(square);              
-    };
+        squareContainer.appendChild(square);    
+    };      
 };
+
+let squareBlock = document.querySelectorAll('.square-block');
+squareBlock.forEach(squareBlock => squareBlock.addEventListener
+    ('mouseenter', e => {
+        squareBlock.style.backgroundColor = 'black';
+    })
+);  
 
 // Color Randomizer
 function getRandomColor() {
@@ -36,27 +45,52 @@ function getRandomColor() {
     return randomColor;
 };
 
-// Grabs the square-block class name
-const squareBlock = document.querySelectorAll('.square-block');
-
-// Applies style whenever the user hovers the mouse over the blocks
-squareBlock.forEach(squareBlock => squareBlock.addEventListener
-    ('mouseenter', e => {
-        squareBlock.style.backgroundColor = getRandomColor();
-        squareBlock.style.transition = 'all 0.2s';
-    })
-);        
-
-// Grabs the reset button 
 const resetButton = document.getElementById('reset-btn');
 //  Resets all squares to default white color
 resetButton.addEventListener('click', e => {
     squareBlock.forEach(squareBlock => {
-        squareBlock.style.backgroundColor = 'white';
-        squareBlock.style.transition = 'all 0.2s';
-    })
-}); 
+        squareBlock.style.backgroundColor = 'white';    
+    });
+});
+
+const randomColorBtn = document.getElementById('random-color-btn');
+
+randomColorBtn.addEventListener('click', randomColor);
+
+function randomColor() {
+    squareBlock.forEach(squareBlock => squareBlock.addEventListener('mouseenter', e=> {
+        squareBlock.style.backgroundColor = getRandomColor();
+        })
+    );
+};
+
+const monoColorBtn = document.getElementById('mono-color-btn');
+
+monoColorBtn.addEventListener('click', monoColor);
+
+function monoColor() {
+    squareBlock.forEach(squareBlock => squareBlock.addEventListener
+        ('mouseenter', e => {
+            squareBlock.style.backgroundColor = 'black';
+        })
+    );  
+};
+
+gridSizeBtn.addEventListener('click', e => {
+    squareContainer.innerHTML = '';
+    squareNumber = prompt('Enter a number between 2-100');
+    squareNumber = createSquares();
+    squareBlock = document.querySelectorAll('.square-block');
+    if(randomColorBtn.clicked == true) {
+        randomColor();
+    } else {
+        monoColor();
+    };
+});
+
     
+
+
 
 
     
